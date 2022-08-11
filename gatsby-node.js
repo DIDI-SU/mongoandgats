ed14c6b898e8a74b8da645028137b940cd8d9155
DIDI-SU <jisoodidi86@gmail.com>
2022-08-11 10:30:32 +0900

    ✨ 왜지?

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,9 +1,29 @@
-exports.createPages = async ({ actions }) => {
+const path = require("path")
+
+exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
-  createPage({
-    path: "/using-dsg",
-    component: require.resolve("./src/templates/using-dsg.js"),
-    context: {},
-    defer: true,
-  })
+
+  const { data } = await graphql(`
+    {
+      books: allMongodbGatsbyBooks {
+        edges {
+          node {
+            id
+          }
+        }
+      }
+    }
+  `)
+
+  const pageTemplate = path.resolve("./src/components/book.js")
+
+  for (const { node } of data.books.edges) {
+    createPage({
+      path: "/book/${node.id}/",
+      component: pageTemplate,
+      context: {
+        id: node.id,
+      },
+    })
+  }
 }
